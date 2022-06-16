a0fd2f89c3a69829e4549796a5a583f12c69425f
stolarz <stolarz92@gmail.com>
2022-06-16 18:52:36 +0200

    Create worker to handle message send by the rabbitmq

---
+++ app/workers/restaurants_worker.rb
@@ -0,0 +1,10 @@
+class RestaurantsWorker
+  include Sneakers::Worker
+
+  from_queue "dashboard.restaurants", env: nil
+  
+  def work(raw_restaurant)
+    Restaurant.push(raw_restaurant)
+    ack! 
+  end
+end
